9fedf076f6c4ca166082ef1e39ef5adcc62dedeb
clubfor5 <444305475@qq.com>
2018-03-19 12:21:26 +0800

    上位机模型，用于烧录AT指令

---
+++ SetTime/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SetTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(61, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "COM1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "时间校准";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(96, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(86, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "更改密码";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "新密码";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(160, 124);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "更改信息";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SetTime/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SetTime
+{
+    public partial class Form1 : Form
+    {
+        SerialPort mySerial = null; //声明串口类
+        bool isOpen = false; //打开串口标志
+        bool comExistence = true; //端口是否存在
+        bool isSetPort = false; //是否设置参数
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            initial();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenCloseCom();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//时间校准
+            string year= ((DateTime.Now.Year)%100).ToString("D2");
+            string month = DateTime.Now.Month.ToString("D2");
+            string day = DateTime.Now.Day.ToString("D2");
+            string week = DateTime.Now.DayOfWeek.ToString();
+            string hour = DateTime.Now.Hour.ToString("D2");
+            string minute = DateTime.Now.Minute.ToString("D2");
+            string second = DateTime.Now.Second.ToString("D2");
+            switch(week)
+            {
+                case "Sunday": week = "0"; break;
+                case "Monday": week = "1"; break;
+                case "Tuesday": week = "2"; break;
+                case "Wednesday": week = "3"; break;
+                case "Thursday": week = "4"; break;
+                case "Friday": week = "5"; break;
+                case "Saturday": week = "6"; break;
+            }
+            string now = year+month+day+week+hour+minute+second;
+            Console.WriteLine("AT+TIME=" + now);
+            SendData("AT+TIME=" + now);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty || textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("请输入用户名和密码");
+                return;
+            }
+            Console.WriteLine("AT+USER=" + textBox1.Text + "," + textBox2.Text);
+            SendData("AT+USER=" + textBox1.Text + "," + textBox2.Text);
+        }
+
+        void initial()
+        {
+            CheckCOM();
+            SetPortProperty(comboBox1.Text, "9600");
+            OpenCloseCom();
+        }
+
+        public void OpenCom()
+        {
+            if (!comExistence) //检测串口设置
+            {
+                Console.WriteLine("Can't find port.");
+                return;
+            }
+
+            if (!isSetPort)
+            {
+                SetPortProperty(comboBox1.Text, "9600");
+                if (!isSetPort) return; 
+            }
+            try
+            {
+                mySerial.Open();
+                isOpen = true;
+                button1.Text = "断开";
+                Console.WriteLine(mySerial.PortName + " Open.");
+            }
+            catch
+            {
+                isOpen = false;
+                button1.Text = "连接";
+                Console.WriteLine(mySerial.PortName + " open failed.");
+            }
+        }
+
+        public void CloseCom()
+        {
+            try
+            {
+                CheckCOM();
+                mySerial.Close();
+                mySerial.Dispose();
+                isOpen = false;
+                button1.Text = "连接";
+                Console.WriteLine(mySerial.PortName + " Closed.");
+            }
+            catch
+            {
+                button1.Text = "断开";
+                Console.WriteLine(mySerial.PortName + " close failed.");
+            }
+        }
+
+        public void OpenCloseCom()
+        {//打开或关闭串口
+            if (isOpen == false) //尚未打开
+            {
+                OpenCom();
+            }
+            else //已打开
+            {
+                CloseCom();
+            }
+        }
+
+        public void CheckCOM()
+        {//检测端口
+            comboBox1.Items.Clear();
+            foreach (string comName in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(comName);
+                comExistence = true;
+            }
+            if (comboBox1.Items.Count > 0) comboBox1.Text = (string)comboBox1.Items[0];
+            if (comboBox1.Items.Count > 1) comboBox1.Text = (string)comboBox1.Items[1];
+        }
+
+        public void SetPortProperty(string comPort = "COM1", string comBaudRate = "9600")
+        { //设置串口属性
+            try
+            {
+                mySerial = new SerialPort();
+                mySerial.PortName = comPort.Trim(); //端口名
+                mySerial.BaudRate = Convert.ToInt32(comBaudRate.Trim()); //波特率
+                mySerial.Parity = Parity.None;
+                mySerial.DataBits = 8;//数据位
+                mySerial.StopBits = StopBits.One;
+
+                mySerial.ReadTimeout = -1;//读取超时时间
+                mySerial.RtsEnable = true;
+                mySerial.DataReceived += new SerialDataReceivedEventHandler(ReceiveEven);
+
+                isSetPort = true;
+            }
+            catch
+            {
+                Console.WriteLine("Setting port failed.");
+            }
+        }
+
+        public void SendData(string DataToSend)
+        {//发送数据
+            
+            if (isOpen)
+            {
+                try
+                {
+                    mySerial.Write(DataToSend);
+                    Console.WriteLine("Sent:" + DataToSend);
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Sending failed.");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Port not open.");
+            }
+        }
+
+        public void ReceiveEven(Object sender, SerialDataReceivedEventArgs e)
+        {//接收数据事件
+            Thread.Sleep(400);//等待接收完毕
+            this.Invoke((EventHandler)(delegate { ReceiveData(); }));
+            //ReceiveData();
+        }
+
+        public void ReceiveData()
+        { //接收数据
+            string DataReceive = string.Empty;
+            Byte[] TempReceive = new Byte[mySerial.BytesToRead];
+            mySerial.Read(TempReceive, 0, TempReceive.Length);
+            foreach (byte temp in TempReceive)
+            {
+                DataReceive += ((char)temp).ToString();
+            }
+            mySerial.DiscardInBuffer();
+            Console.Write("Received[");
+            Console.Write(DataReceive);
+            Console.WriteLine("]");
+        }
+
+    }
+}
